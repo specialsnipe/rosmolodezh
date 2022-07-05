56840c1664dbc5b158060051284e7a589b356b18
SpecialSnipe <specialsnipe@mail.ru>
2022-07-05 16:57:12 +0500

    Make admin panel. make AdminController and add to user avatar and login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// TODO: админ панельные дела
+
+Route::group(['as' => 'admin.', 'prefix' => 'admin'], function () {
+    Route::get('/', [\App\Http\Controllers\AdminController::class, 'index'])->name('dashboard');
+    Route::resource('settings', \App\Http\Controllers\SettingController::class);
+    Route::resource('posts', \App\Http\Controllers\PostController::class);
+    Route::resource('users', \App\Http\Controllers\UserController::class);
 });
+
+// TODO: Регистрацонные дела
+
+// TODO: Студенческие дела
