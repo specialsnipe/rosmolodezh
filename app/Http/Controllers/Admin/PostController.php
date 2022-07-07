618508e94aedeca83a94cd4c05d43a74810bf029
SpecialSnipe <specialsnipe@mail.ru>
2022-07-07 10:20:01 +0500

    transfer PostController into App\Http\Controllers\Admin

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StorePostRequest;
+use App\Http\Requests\Post\UpdatePostRequest;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
+    }
+
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+
+    public function store(StorePostRequest $request)
+    {
+        $data = $request->validated();
+        Post::firstOrCreate($data);
+        return redirect()->route('admin.posts.index');
+    }
+
+    public function show(Post $post)
+    {
+        return view('admin.posts.show', compact('post'));
+    }
+
+
+    public function edit(Post $post)
+    {
+        return view('admin.posts.edit', compact('post'));
+    }
+
+
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        //
+    }
+
+
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
