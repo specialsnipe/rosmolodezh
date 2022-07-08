c679f2849772faff6ac82d66384787be1b6766d4
NikitaMurugov <idesignerdisk2@yandex.ru>
2022-07-08 15:52:33 +0500

    some fix

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,29 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Services\ResizeImage;
 use App\Http\Requests\User\StoreUserRequest;
-use App\Http\Requests\User\UpdateUserRequest;
 use App\Models\Gender;
 use App\Models\Occupation;
 use App\Models\Role;
 use App\Models\User;
 
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
 
-    /**
-     * Show all users on admin panel
-     *
-     * @return Factory|View|Application
-     */
     public function index()
     {
         $users = User::all();
@@ -34,8 +25,7 @@ class UserController extends Controller
 
 
     /**
-     * Show the form to create custom user
-     * @return Application|Factory|View
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
@@ -45,9 +35,8 @@ class UserController extends Controller
         return view('admin.users.create', compact('genders', 'roles', 'occupations'));
     }
 
+
     /**
-     * Store the user created by admin
-     *
      * @param Request $request
      * @return RedirectResponse
      */
@@ -56,81 +45,58 @@ class UserController extends Controller
         $data = $request->validated();
 
         $data['password'] = Hash::make($data['password']);
-//        dd($request->file('file'));
-        $filename = ResizeImage::make($request->file('file'), 'users/avatars', 150, 150);
+        $file = $data['file'];
+        unset($data['file']);
+        $file = Storage::disk('public')->put('/images', $file);
+        $data['avatar'] = Storage::url($file);
 
-        $data['avatar'] = $filename;
 
         $user = User::firstOrCreate($data);
         return redirect()->route('admin.users.index')->withInput(['user' => $user]);
     }
 
+
     /**
-     * Show the user
-     *
      * @param User $user
-     * @return Application|Factory|View
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function show(User $user)
     {
-        return view('admin.users.show', compact('user'));
+        return view('admin.users.show',compact('user'));
     }
 
     /**
-     * Show the form for editing the user resource.
+     * Show the form for editing the specified resource.
      *
      * @param User $user
-     * @return Application|Factory|View
+     * @return \Illuminate\Http\Response
      */
     public function edit(User $user)
     {
-        $genders = Gender::all();
-        $roles = Role::all();
-        $occupations = Occupation::all();
-        return view('admin.users.edit', compact('user', 'genders', 'roles', 'occupations'));
+        //
     }
 
     /**
-     * Update the user resource in storage.
+     * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return RedirectResponse
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function update(UpdateUserRequest $request, User $user)
+    public function update(Request $request, User $user)
     {
-        $data = $request->validated();
-        try {
-
-            if (!$request->hasFile('file')) {
-
-                $user->updateOrFail($request->validated());
-
-            }
-            $filename = ResizeImage::make($request->file('file'), 'users/avatars', 150, 150);
-            $data['avatar'] = $filename;
-            $user->updateOrFail($data);
-        } catch (\Exception $exception) {
-            return dd($exception);
-        }
-        return redirect()->route('admin.users.show', $user->id);
+        //
     }
 
     /**
-     * Remove the user resource from storage.
+     * Remove the specified resource from storage.
      *
-     * @param int $id
-     * @return RedirectResponse
-     * @throws \Throwable
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
     public function destroy(User $user)
     {
-        try {
-            $user->deleteOrFail();
-            return redirect()->route('admin.users.index');
-        } catch (\Exception $exception) {
-            return dd($exception);
-        }
+        //
     }
 
 }
