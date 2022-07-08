01df374231a1400e4a6ba6fb749fcf97d565486b
DexNick0145 <idesignerdisk2@yandex.ru>
2022-07-08 21:59:16 +0500

    add vue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: command === 'serve' ? '' : '/build/',
+    publicDir: 'fake_dir_so_nothing_gets_copied',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        },
+    },
     plugins: [
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
         ]),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        })
     ],
 });
