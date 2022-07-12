781d3c87b5fb19b007a6098e938dc80c3b41d4d3
NikitaMurugov <idesignerdisk2@yandex.ru>
2022-07-12 13:34:38 +0500

    delete vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,1 @@
 import './bootstrap';
-import { createApp } from "vue";
-import TrackBlockItem from "./components/Admin/TrackBlockItem.vue";
-import TrackItem from "./components/Admin/TrackItem.vue";
-
-const app = createApp({})
-app.component('track-item', TrackItem)
-app.component('track-block-item', TrackBlockItem)
-app.mount('#api')
