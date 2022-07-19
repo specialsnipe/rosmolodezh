eabe9a5c5e901e3c8b0809dd8cf4f5d42382b97f
SpecialSnipe <specialsnipe@mail.ru>
2022-07-19 12:14:28 +0500

    fix admin

--- resources/views/admin/main/index.blade.php
+++ resources/views/admin/main/index.blade.php
@@ -58,7 +58,7 @@ use App\Models\Post;
                             <div class="icon">
                                 <i class="ion ion-person-add"></i>
                             </div>
-                            <a href="{{   }}" class="small-box-footer">Все пользователи <i class="fas fa-arrow-circle-right"></i></a>
+                            <a href="#" class="small-box-footer">Все пользователи <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
 
