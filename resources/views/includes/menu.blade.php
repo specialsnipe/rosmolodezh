9e4719e6ad664a8f6b3e7ab0890cd1a16d78c8c2
SMY99 <artem.mosin@inbox.ru>
2022-08-02 12:36:53 +0500

    fix

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -9,9 +9,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a href="{{ route('search') }}" class="nav-link"> Поиск по сайту <svg style="width: 20px; height:20px" fill="rgba(0,0,0,0.4)" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 32 32" id="Glyph" version="1.1" viewBox="0 0 32 32" xml:space="preserve"><path d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z" id="XMLID_223_"/></svg></a>
-                </li>
+                
                 <li class="nav-item">
                     <a class="nav-link @if(Request::route()->getName() == 'home') active @endif " aria-current="page"
                         href="{{ route('home') }}">Главная</a>
@@ -27,7 +25,7 @@
                         <li><a class="dropdown-item" href="{{ route('about') }}">Страница о проекте</a></li>
                     </ul>
                 </li>
-                <li class="nav-item ">
+                <li class="nav-item">
                     <a class="nav-link  @if(Request::route()->getName() == 'tracks.index') active @endif"
                         aria-current="page" href="{{ route('tracks.index') }}">Направления</a>
                 </li>
@@ -54,8 +52,11 @@
                     </ul>
                 </li>
                 @else
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center @if(Request::route()->getName() == 'search') active @endif " href="{{ route('search') }}" class="nav-link"> Поиск по сайту <i class="fas fa-search"></i></a>
+                </li>
                 <li class="nav-item ">
-                    <a class="nav-link d-flex align-items-center" aria-current="page" href="{{ route('auth.login') }}">Войти <i class="fas fa-sign-in"></i></a>
+                    <a class="nav-link d-flex align-items-center  @if(Request::route()->getName() == 'auth.login') active @endif" aria-current="page" href="{{ route('auth.login') }}">Войти <i class="fas fa-sign-in"></i></a>
                 </li>
 
                 @endauth
