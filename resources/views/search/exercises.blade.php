101a40f83428f4c9bba027ddce887a5781694190
specialsnipe@mail.ru <M1J2$Ho%E>
2022-08-09 12:56:42 +0500

    fix serch/exercises search/posts

--- resources/views/search/exercises.blade.php
+++ resources/views/search/exercises.blade.php
@@ -13,11 +13,13 @@
 
     <div class="container search-container">
         <p class="h1-content">Поиск по сайту</p>
-                <form action="{{route('exercises.search')}}" method="get" class="d-flex justify-content-between form-floating col-sm-12 col-md-10 col-lg-12">
-                    <input type="text" class="mr-3 form-control" name="search" value = "{{request('search')}}" placeholder="search">
-                    <label for="floatingInput">Введите текст</label>
-                    <button class=" search col-sm-12 col-md-2 col-lg-2">Искать</button>
-                </form>
+        <form action="{{route('exercises.search')}}" method="get"
+              class="d-flex justify-content-between form-floating col-sm-12 col-md-10 col-lg-12">
+            <input type="text" class="mr-3 form-control" name="search" value="{{request('search')}}"
+                   placeholder="search">
+            <label for="floatingInput">Введите текст</label>
+            <button class=" search col-sm-12 col-md-2 col-lg-2">Искать</button>
+        </form>
         <div class="mt-2">
             <a class="btn btn-primary" role="tab" href="{{route('search', ['search'=>request('search')])}}">Все</a>
 
@@ -29,22 +31,26 @@
             <div class="row">
                 @if($exercises)
                     @forelse($exercises as $exercise)
-                            <a href="{{route('blocks.exercises.show',[$exercise->block, $exercise->id])}}" class="col-sm-12 col-md-5 search-item" style="text-decoration: none">
-                                <span class="text-muted">Упражнение</span> <p class="h1">{{$exercise->title}} </p>
-                                <p>{!! str_replace("$search","<b>$search</b>", $exercise->body) !!}</p>
-                            </a>
+                        <a href="{{route('blocks.exercises.show',[$exercise->block, $exercise->id])}}"
+                           class="col-sm-12 col-md-5 search-item" style="text-decoration: none">
+                            <span class="text-muted">Упражнение</span>
+                            <p class="h1">{{$exercise->title}} </p>
+                            <p>{!! str_replace("$search","<b>$search</b>", $exercise->body) !!}</p>
+                        </a>
 
                     @empty
                         <p>Ничего не нашлось</p>
                     @endforelse
                 @else
-                    <img src="{{asset('storage/search/search.png')}}" class="mt-3"id="img" alt="Поиcк">
+                    <img src="{{asset('storage/search/search.png')}}" class="mt-3" id="img" alt="Поиcк">
                 @endif
             </div>
         </div>
-        <div class="card m-3 p-3">
-            {{ $exercises->withQueryString()->links() }}
-        </div>
+        @if(isset($exercises))
+            <div class="card m-3 p-3">
+                {{ $exercises->withQueryString()->links() }}
+            </div>
+        @endif
     </div>
 
 @endsection
