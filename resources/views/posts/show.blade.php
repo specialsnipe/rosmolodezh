beec0a3c52603121f32ff9775c81e8404b9bd7f4
NikitaMurugov <idesignerdisk2@yandex.ru>
2022-09-15 16:41:34 +0500

    fix

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
 @endpush
 
 @push('seo')
-<meta name='keywords' content='{{ $post->keywords ?? 'Новость' }}'>
+<meta name='keywords' content='{{ $post->keywords ?? ' Новость' }}'>
 {!! seo()->for($post) !!}
 @endpush
 
@@ -20,14 +20,16 @@
                         <div class="slider__wrapper">
                             <div class="slider__items">
                                 @foreach ($post->images as $image)
-                                    <div class="slider__item" style="max-height: 500px">
-                                        <img src="{{ $image->imageMedium }}"  class="img-fluid rounded w-100"  alt="">
-                                    </div>
+                                <div class="slider__item" style="max-height: 500px">
+                                    <img src="{{ $image->imageMedium }}" class="img-fluid rounded w-100" alt="">
+                                </div>
                                 @endforeach
                             </div>
 
-                    <a class="slider__control slider__control_prev" href="#" role="button" data-slide="prev"></a>
-                    <a class="slider__control slider__control_next" href="#" role="button" data-slide="next"></a>
+                            <a class="slider__control slider__control_prev" href="#" role="button"
+                                data-slide="prev"></a>
+                            <a class="slider__control slider__control_next" href="#" role="button"
+                                data-slide="next"></a>
                         </div>
                     </div>
 
@@ -36,59 +38,60 @@
                     <img src="{{ asset($post->images[0]->imageMedium) }}" class="img-fluid rounded" alt="">
                     @endif
                 </div>
-                    <div class="col-sm-12 col-md-4">
-                        <div class="">
-                            <div class="h2">Другие интересные статьи</div>
-                            <div class="line mt-3 mb-0"></div>
-                            <div class="link-content">
-                                @foreach ($posts as $postx)
+                <div class="col-sm-12 col-md-4">
+                    <div class="">
+                        <div class="h2">Другие интересные статьи</div>
+                        <div class="line mt-3 mb-0"></div>
+                        <div class="link-content">
+                            @foreach ($posts as $postx)
+                            <a href="{{ route('posts.show', $postx->id) }}" class="">
                                 <div class="card mt-3">
                                     <div class="card-body">
-                                        <a href="{{ route('posts.show', $postx->id) }}" class="">
-                                            <p class="h5">{{ $postx->title }}</p>
-                                            <p class="text-truncate">{{ $postx->excerpt }}</p>
-                                        </a>
+                                        <p class="h5">{{ $postx->title }}</p>
+                                        <p class="text-truncate">{{ $postx->excerpt }}</p>
                                     </div>
                                 </div>
-                                @endforeach
-                            </div>
+                            </a>
+
+                            @endforeach
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div class="col-12 mt-3">
-                    <span class="h2">{{ $post->title }}</span>
-                    <p class="mt-3">{!! $post->body !!}</p>
-                    <div class="line mt-2 mb-2"></div>
+            <div class="col-12 mt-3">
+                <span class="h2">{{ $post->title }}</span>
+                <p class="mt-3">{!! $post->body !!}</p>
+                <div class="line mt-2 mb-2"></div>
 
-                    <p class="h2">Полезные ссылки:</p>
-                    <div class="row">
-                        <a href="" class="col-12 link-text">https://habr.com/ru/post/675130/</a>
-                        <a href="" class="col-12 link-text">https://habr.com/ru/post/675130/</a>
-                        <a href="" class="col-12 link-text">https://habr.com/ru/post/675130/</a>
-                        <a href="" class="col-12 link-text">https://habr.com/ru/post/675130/</a>
-                    </div>
-
+                <p class="h2">Полезные ссылки:</p>
+                <div class="row">
+                    <a href="" class="col-12 link-text">https://habr.com/ru/post/675130/</a>
+                    <a href="" class="col-12 link-text">https://habr.com/ru/post/675130/</a>
+                    <a href="" class="col-12 link-text">https://habr.com/ru/post/675130/</a>
+                    <a href="" class="col-12 link-text">https://habr.com/ru/post/675130/</a>
                 </div>
+
             </div>
         </div>
-
-        @guest
-        <div class="screen register-screen">
-            <x-registration-form></x-registration-form>
-        </div>
-        @endguest
     </div>
+
+    @guest
+    <div class="screen register-screen">
+        <x-registration-form></x-registration-form>
+    </div>
+    @endguest
+</div>
 </div>
 
 @endsection
 @push('script')
-    <script defer>
-        const sldier = new SimpleAdaptiveSlider('.slider', {
+<script defer>
+    const sldier = new SimpleAdaptiveSlider('.slider', {
             loop: true,
             autoplay: false,
             interval: 5000,
             swipe: true,
         });
-    </script>
+</script>
 @endpush
