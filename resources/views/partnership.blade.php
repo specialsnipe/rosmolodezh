4068a5cddc7306640d18004d3052ab2c9ad18ffb
NikitaMurugov <idesignerdisk2@yandex.ru>
2022-10-20 21:11:47 +0500

    upd: animation with show all information

--- resources/views/partnership.blade.php
+++ resources/views/partnership.blade.php
@@ -4,24 +4,42 @@
 <link rel="stylesheet" href="{{ asset('css/media.css') }}">
 
 <style>
+
     .show-more  {
         text-decoration: none;
         cursor: pointer;
         color: inherit;
-    }.text-about  {
+    }
+    .text-about  {
         max-width: 70%;
     }
     .text-truncated {
-        display: -webkit-box;
-        -webkit-line-clamp: 3;
-        -webkit-box-orient: vertical;
+        overflow: hidden;
+        height: 70px;
+    }
+    .text-about {
+        transition: height 0.2s;
+        -moz-transition: height 0.2s; /* Firefox 4 */
+        -webkit-transition: height 0.2s; /* Safari and Chrome */
+        -o-transition: height 0.2s; /* Opera */
+    }
+    .text-showed {
+        height: auto !important;
         overflow: hidden;
     }
     .content-body {
-        transition: height 200ms;
+        max-height: auto;
+        z-index: 2;
     }
-    .content-info {
-        transition: height 200ms;
+    .bg-icon {
+        position: absolute;
+        top:0;
+        width: 100%;
+        height: 200px;
+        z-index: 1;
+    }
+    .phrase-bg-img-2 {
+        transition: 200ms;
     }
 </style>
 @endpush
@@ -31,26 +49,27 @@
 <div class="container">
         <div class="main-container-directions">
             <h1 class="h1-content mt-0 mb-4">ИНФОРМАЦИЯ ПАРТНЁРАМ</h1>
-            <div class="row gy-4 gx-5 d-flex justify-content-center">
-                <div class="col-md-10">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-12">
                     <div class="card content-info">
-                    <div class="card-body content-body">
-                        <h5 class="card-title">Заголовок</h5>
-                        <p class="card-text text-truncated ">Если ваши клиенты подключают наши продукты и сервисы, платим вам до 60 000 ₽ за каждого</p>
-                        <div class="phrase-bg-img-1"></div>
-                    </div>
-
+                        <div class="card-body content-body">
+                            <h5 class="card-title">Заголовок</h5>
+                            <p class="card-text">Если ваши клиенты подключают наши продукты и сервисы, платим вам до 60 000 ₽ за каждого</p>
+                        </div>
+                        <div class="bg-icon w-100">
+                            <div class="phrase-bg-img-1"></div>
+                        </div>
                     </div>
                 </div>
             </div>
 <!--
             <h2 class="h1-content mt-3 mb-4">ИНФОРМАЦИЯ ПАРТНЁРАМ</h2> -->
 
-            <div class="row gy-4 gx-5 d-flex justify-content-center mt-1">
-                <div class="col-md-5">
+            <div class="row d-flex justify-content-center mt-1">
+                <div class="col-md-6">
                     <div class="card content-info">
 
-                    <div class="card-body ">
+                    <div class="card-body content-body">
 
                         <h5 class="card-title">Заголовок</h5>
                         <p class="text-about text-truncated card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -63,23 +82,31 @@
                         </p>
 
                         <a class="show-more">Подробнее</a>
+
+                    </div>
+
+                    <div class="bg-icon w-100">
                         <div class="phrase-bg-img-2"></div>
                     </div>
                     </div>
                 </div>
-                <div class="col-md-5">
+                <div class="col-md-6">
                     <div class="card content-info">
-                    <div class="card-body">
-                        <h5 class="card-title">Заголовок</h5>
+                        <div class="card-body content-body">
+                            <h5 class="card-title">Заголовок</h5>
 
-                        <p class="text-about text-truncated card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet,
-                            nulla et dictum interdum, nisi lorem egestas vitae scel erisque enim ligula venenatis dolor. Maecenas nisl
-                            est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis
-                            dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis.
-                            Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</p>
+                            <p class="text-about text-truncated card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet,
+                                nulla et dictum interdum, nisi lorem egestas vitae scel erisque enim ligula venenatis dolor. Maecenas nisl
+                                est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis
+                                dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis.
+                                Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</p>
 
-                        <a class="show-more">Подробнее</a>
+                            <a class="show-more">Подробнее</a>
+                        </div>
+
+                    <div class="bg-icon w-100">
                         <div class="phrase-bg-img-2"></div>
+                    </div>
                     </div>
                     </div>
                 </div>
@@ -98,7 +125,10 @@
         moreButtons.forEach((button) => {
             button.addEventListener('click',(event)=>{
                 let text = button.parentNode.querySelector('.text-about');
-                text.classList.toggle('text-truncated');
+                let content = button.parentNode;
+                text.style.height = text.style.height.split('px')[0] == text.scrollHeight
+                    ? "70px"
+                    : text.scrollHeight + "px";
                 button.innerHTML = button.innerHTML === 'Подробнее' ? 'Скрыть' : 'Подробнее';
             });
         });
