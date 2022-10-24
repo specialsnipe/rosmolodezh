9ae5b073b49dcc93594f8ac8bef49ffcfd5099d2
NikitaMurugov <idesignerdisk2@yandex.ru>
2022-10-24 14:11:32 +0500

    upd: edit contact page

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -17,7 +17,7 @@
                     <i class="fas fa-map-marker-alt"></i>
                     <div class="card-body">
                         <h5 class="card-title">Наш адрес</h5>
-                        <p class="card-text">{{$settings->location_description}}</p>
+                        <p class="card-text" style="min-height: 50px">{{$settings->location_description}}</p>
                     </div>
                     <div class="line mt-1 mb-1 mt-1 mb-1"></div>
 
@@ -31,7 +31,7 @@
                     <i class="fab fa-vk"></i>
                     <div class="card-body">
                         <h5 class="card-title">Мы в Вконтакте</h5>
-                        <p class="card-text">{{$settings->vk_description}}</p>
+                        <p class="card-text" style="min-height: 50px">{{$settings->vk_description}}</p>
                     </div>
                     <div class="line mt-1 mb-1"></div>
                     <a target="_blank" href="{{$settings->vk_url}}">Группа вконтакте</a>
@@ -50,7 +50,7 @@
                     </svg>
                     <div class="card-body">
                         <h5 class="card-title">Мы в Дзене</h5>
-                        <p class="card-text">{{$settings->zen_description}}</p>
+                        <p class="card-text" style="min-height: 50px">{{$settings->zen_description}}</p>
                     </div>
                     <div class="line mt-1 mb-1"></div>
                     <a target="_blank" href="{{$settings->zen_url}}">Перейти в Дзен</a>
@@ -66,19 +66,22 @@
                     </div>
                     <div class="row">
                         @foreach($settings->telegrams as $telegram)
-                        <div class="col mt-3">
+                        <div class="col-12 mt-3">
                             <a class="text-decoration-none" target="_blank"
                             href="https://t.me/{{ $telegram->username }}">
-                            <div class="card">
-                                <span class="text-center">
+                            <div class="card text-left">
+                                <div class="w-100">
+                                <span class="mr-3">
                                     <svg class="pr-2"
                                         style="width:30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
                                         <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
                                         <path
                                             d="M248,8C111.033,8,0,119.033,0,256S111.033,504,248,504,496,392.967,496,256,384.967,8,248,8ZM362.952,176.66c-3.732,39.215-19.881,134.378-28.1,178.3-3.476,18.584-10.322,24.816-16.948,25.425-14.4,1.326-25.338-9.517-39.287-18.661-21.827-14.308-34.158-23.215-55.346-37.177-24.485-16.135-8.612-25,5.342-39.5,3.652-3.793,67.107-61.51,68.335-66.746.153-.655.3-3.1-1.154-4.384s-3.59-.849-5.135-.5q-3.283.746-104.608,69.142-14.845,10.194-26.894,9.934c-8.855-.191-25.888-5.006-38.551-9.123-15.531-5.048-27.875-7.717-26.8-16.291q.84-6.7,18.45-13.7,108.446-47.248,144.628-62.3c68.872-28.647,83.183-33.623,92.511-33.789,2.052-.034,6.639.474,9.61,2.885a10.452,10.452,0,0,1,3.53,6.716A43.765,43.765,0,0,1,362.952,176.66Z" />
                                     </svg>
-                                    {{ '@'.$telegram->username }} <br>
-                                    {{ $telegram->description }}</span>
+                                </span>
+                                <span>{{ '@'.$telegram->username }} </span>
+                                <span class="text-black"> - {{ $telegram->description }}</span>
+                            </div>
                             </div>
                         </a>
                         </div>
