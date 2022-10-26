afb294ca38eddedb5b0aec00e16837ddea2496d5
NikitaMurugov <idesignerdisk2@yandex.ru>
2022-10-26 20:10:49 +0500

    upd: fix track showing with deleted user

--- app/Http/Controllers/Client/ProfileTrackController.php
+++ app/Http/Controllers/Client/ProfileTrackController.php
@@ -11,9 +11,20 @@ class ProfileTrackController extends Controller
     public function show(Track $track)
     {
         $requestsToJoin = $track->users_requests->where('action', 'send')->where('joining', true);
+        $requestsToJoin = $this->filterNullableUser($requestsToJoin);
+
         $requestsToRefused = $track->users_requests->where('action', 'send')->where('refused', true);
+        $requestsToRefused = $this->filterNullableUser($requestsToRefused);
+
         $users = $track->usersWithoutTrashed;
         return view('profile.track.teacher.show', compact('track', 'requestsToJoin', 'requestsToRefused', 'users'));
+    }
+
+    public function filterNullableUser($collection)
+    {
+        return $collection->filter(function($item) {
+            return $item->user !== null;
+        });
     }
 
     public function userShow(Track $track)
