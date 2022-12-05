0d0fc34c5d39ed23556e6d45a343477dbe5c4444
DexNick0145 <idesignerdisk2@yandex.ru>
2022-12-05 20:53:21 +0500

    some fix

--- resources/views/admin/tracks/index.blade.php
+++ resources/views/admin/tracks/index.blade.php
@@ -105,12 +105,12 @@
                                         </tr>
                                         <tr>
                                             <td>Успеваемость:</td>
-                                            <td><span class="status_block status_success">100%</span></td>
+                                            <td><span class="status_block status_success">{{ $track->averageScore }}</span></td>
                                         </tr>
                                         <tr>
                                             <td>Средний балл:</td>
                                             <td><span
-                                                    class="status_block status_success">{{$allAverageMark[$loop->index]}}</span>
+                                                    class="status_block status_success">{{ $track->academicPerformance }}</span>
                                             </td>
                                         </tr>
                                     </table>
