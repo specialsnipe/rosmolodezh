9c8cca91a6e2902e96022ff4a72895a6dc1b45ad
NikitaMurugov <idesignerdisk2@yandex.ru>
2023-06-06 15:56:53 +0500

    fix footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,7 +3,7 @@
 use App\Models\Track;
 use App\Models\Information;
     $information = Information::all()[0];
-    $tracks = Track::limit(3)->get();
+    $tracks = Track::all();
 
 @endphp
 <footer class="text-center text-muted" style="color: #fff!important;">
@@ -71,19 +71,6 @@ use App\Models\Information;
           <span class="me-4">Присоединяйтесь к нам в социальных сетях:</span>
           <a target="_blank" href="{{ $information->vk_url }}" class="me-4 icon">
             <i class="fab fa-vk"></i>
-          </a>
-          <a target="_blank" href="{{ $information->tg_url }}" class="me-4 icon">
-            <i class="fab fa-telegram-plane"></i>
-          </a>
-          <a target="_blank" href="{{ $information->zen_url }}" class="me-4 icon">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="22" viewBox="0 0 56 56" fill="none">
-                <path
-                    d="M0 28C0 12.536 12.536 0 28 0C43.464 0 56 12.536 56 28C56 43.464 43.464 56 28 56C12.536 56 0 43.464 0 28Z"
-                    fill="white" />
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M27.4334 0C27.3011 11.5194 26.5478 17.9662 22.257 22.257C17.9662 26.5478 11.5194 27.3011 0 27.4334V29.1051C11.5194 29.2373 17.9662 29.9906 22.257 34.2814C26.4805 38.5049 27.2766 44.8173 27.4267 56H29.1118C29.2618 44.8173 30.0579 38.5049 34.2814 34.2814C38.5049 30.0579 44.8173 29.2618 56 29.1118V27.4266C44.8173 27.2766 38.5049 26.4805 34.2814 22.257C29.9906 17.9662 29.2373 11.5194 29.1051 0H27.4334Z"
-                    fill="#404040" />
-            </svg>
           </a>
         </div>
         <div class="col-md-4 mx-auto ">
